c1b0333fa9eb590f29f160cdc8bae5bcba577419
andyyoshikawa <yoshikawa0121@icloud.com>
2021-12-03 15:13:48 +0900

    feat: 採点用とリネームの機能を分割

--- Scoring/score.py
+++ Scoring/score.py
@@ -5,42 +5,62 @@ import pathlib
 import shutil
 import openpyxl
 
-if __name__ == '__main__':
-  # ファイルのrename
-  # files = glob.glob("./prog2/*.c")
 
-  # student_num_list = list(map(lambda file: file.split()[0].replace("./prog2/", ""), files))
-  # for index, file in enumerate(files):
-  #   os.rename(file, f"./prog2/{student_num_list[index]}.c")
+def rename_files():
+    # ファイルのrename
+    files_dir = "prog2"
+    files_extension = ".c"
 
-  # ファイルの採点
+    files = glob.glob(f"./{files_dir}/*{files_extension}")
 
-  files = glob.glob("./prog2/*.c")
-  input_files = glob.glob("./src/*.txt")
-  wb = openpyxl.load_workbook("saiten.xlsx")
-  ws = wb["4回プログラミング演習"]
+    student_num_list = list(
+        map(lambda file: file.split()[0].replace("./{files_dir}/", ""), files)
+    )
+    for index, file in enumerate(files):
+        os.rename(file, f"./{files_dir}/{student_num_list[index]}{files_extension}")
 
-  for student_index, file in enumerate(files):
-    # 学籍番号をシートに書き込み
-    cell = ws.cell(row=student_index + 1, column=1)
-    cell.value = file.replace('./prog2/', '').replace('.c', '')
 
-    for input_index, input in enumerate(input_files):
-      shutil.copy(input, './input.txt')
-      os.system("gcc %s" % file)
-      # 正しくコンパイルされ a.outがあるか
-      if os.path.exists('./a.out'):
-        result = subprocess.Popen('./a.out', stdout=subprocess.PIPE).communicate()[0].decode('utf-8')
+def calc_score():
+    files_dir = "prog2"
+    files_extension = ".c"
+    src_dir = "src"
+    excel_file = "saiten.xlsx"
+    excel_sheet_name = "4回プログラミング演習"
+    files = glob.glob(f"./{files_dir}/*{files_extension}")
+    input_files = glob.glob(f"./{src_dir}/*.txt")
+    wb = openpyxl.load_workbook(excel_file)
+    ws = wb[excel_sheet_name]
 
-        # 出力をシートに書き込み
-        output_cell = ws.cell(row=student_index + 1, column=input_index + 2)
-        output_cell.value = result
-        # a.outを削除
-        os.remove('./a.out')
-      else:
-        # 出力をシートに書き込み
-        output_cell = ws.cell(row=student_index + 1, column=input_index + 2)
-        output_cell.value = "compile was failed"
-  
-  wb.save("saiten.xlsx")
-  wb.close()
+    for student_index, file in enumerate(files):
+        # 学籍番号をシートに書き込み
+        cell = ws.cell(row=student_index + 1, column=1)
+        cell.value = file.replace(f"./{files_dir}/", "").replace(files_extension, "")
+
+        for input_index, input in enumerate(input_files):
+            shutil.copy(input, "./input.txt")
+            os.system("gcc %s" % file)
+            # 正しくコンパイルされ a.outがあるか
+            if os.path.exists("./a.out"):
+                result = (
+                    subprocess.Popen("./a.out", stdout=subprocess.PIPE)
+                    .communicate()[0]
+                    .decode("utf-8")
+                )
+
+                # 出力をシートに書き込み
+                output_cell = ws.cell(row=student_index + 1, column=input_index + 2)
+                output_cell.value = result
+                # a.outを削除
+                os.remove("./a.out")
+            else:
+                # 出力をシートに書き込み
+                output_cell = ws.cell(row=student_index + 1, column=input_index + 2)
+                output_cell.value = "compile was failed"
+
+    wb.save(excel_file)
+    wb.close()
+
+
+if __name__ == "__main__":
+    # moodle からダウンロードしたファイルをrenameしたいとき
+    rename_files()
